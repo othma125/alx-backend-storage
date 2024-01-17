0c53c1463d0b6eb358a03727459a172098d4737c
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-17 15:00:41 +0100

    4-store.sql

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+DROP TRIGGER IF EXISTS reduce_quantity;
+DELIMITER //
+CREATE TRIGGER after_order_insert
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
+//
+DELIMITER ;
