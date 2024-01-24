ac2054d6dcd248aa9a9b44db16bfe083eda6b7ab
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-24 14:43:09 +0100

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -27,6 +27,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Method that stores cache data"""
         key = str(uuid4())
