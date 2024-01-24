8260384e9eb2f6dc05398b9556bc4f067925ee4b
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-24 20:07:57 +0100

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -50,7 +50,7 @@ def replay(fn: Callable) -> None:
     if not isinstance(r, redis.Redis):
         return
     fn_name = fn.__qualname__
-    count: int = r.get(fn_name) if r.exists(fn_name) else 0
+    count: int = int(r.get(fn_name)) if r.exists(fn_name) else 0
     print(f'{fn_name} was called {count} times:')
     inputs = r.lrange(f'{fn_name}:inputs', 0, -1)
     outputs = r.lrange(f'{fn_name}:outputs', 0, -1)
