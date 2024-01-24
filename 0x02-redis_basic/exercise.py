72c697041fed8fd8335a7a1e95461444cf4f879f
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-24 14:31:25 +0100

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Cache module"""
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -16,3 +16,18 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None)
+    -> Union[str, bytes, int, float]:
+        """Method that gets cache data"""
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, key: str) -> str:
+        """Method that gets cache data as string"""
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """Method that gets cache data as int"""
+        return self.get(key, int)
